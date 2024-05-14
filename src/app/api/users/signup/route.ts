da7c52120b6e34bacdb7c28d47ee5ad7ad3b4a99
cookieeszn <klauscity01@gmail.com>
2024-05-14 07:03:10 -0700

    first commit

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,46 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    if (!username || !email || !password) {
+      return NextResponse.json(
+        { error: "Missing required fields (username, email or password)" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User with this email already exists" },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+
+    return NextResponse.json({
+      message: "User created successfully",
+      succes: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
